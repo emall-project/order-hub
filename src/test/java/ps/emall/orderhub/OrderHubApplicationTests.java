03c1454b988d8f314b482cf8ebcb21b6a0a99427
jehadhamid <jehadadhame@gmail.com>
2026-01-27 19:56:41 +0200

    initial infrastructure

---
+++ src/test/java/ps/emall/orderhub/OrderHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ps.emall.orderhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
